a0a1e4c7eaf75d15add26e1ada09698212715e59
Masakorala, Manoj <Manoj.Masakorala@pearson.com>
2019-08-12 10:26:59 +0530

    Updates

---
+++ precache-manifest.95e02134ff66ae64980d411d6d9f77fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d05440fa7e15cb875b7ce39a2dd49c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c08248003a6026f5dab1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e84c79d5e754f66ed6cf",
+    "url": "/static/js/2.3e795878.chunk.js"
+  },
+  {
+    "revision": "c08248003a6026f5dab1",
+    "url": "/static/js/main.d2b86327.chunk.js"
+  },
+  {
+    "revision": "1fc936b2439ada0b6406",
+    "url": "/static/js/runtime~main.61a1272d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
